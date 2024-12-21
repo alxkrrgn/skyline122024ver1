087fa2d1805366b470c242978cb1fd56755e97f0
alxkrrgn <alxkrgn@hotmail.com>
2024-12-21 15:58:35 -0800

    fixed react vesion and reset

--- src/components/PoweredBy.js
+++ src/components/PoweredBy.js
@@ -5,15 +5,13 @@ import '../styles/poweredby.css'; // Import the CSS file specific to About compo
 
 function PoweredBy() {
 
-
+  useEffect(() => {
     document.addEventListener("DOMContentLoaded", () => {
 
-      useEffect(() => {
         const logosContainer = document.querySelector(".logos");
 
         if (logosContainer) {
         const clonedLogos = logosContainer.innerHTML; // Clone the content
-
         logosContainer.innerHTML += clonedLogos; // Append the cloned content
 
       } else {
@@ -26,17 +24,17 @@ function PoweredBy() {
     
     <div className="powered-by">
   <h2>Powered By:</h2>
-  <div className="logos">
-    <img src="/images/Tableau_Logo_PNG4.png" alt="Python" />
-    <img src="/images/Github_logo_PNG2.png" alt="GitHub" /> 
-    <img src="/images/Python-Logo-4.svg" alt="Tableau" />
-    <img src="/images/Ubuntu-Logo-4.svg" alt="ubuntu" />
-    <img src="/images/IBM-Logo-2.svg" alt="IBM" /> 
-    <img src="/images/Gitlab-logo-1.svg" alt="GitLab" /> 
-    <img src="/images/ChatGPT-logo-5.svg" alt="ChatGPT" /> 
+    <div className="logos">
+      <img src="/images/Tableau_Logo_PNG4.png" alt="Python" />
+      <img src="/images/Github_logo_PNG2.png" alt="GitHub" /> 
+      <img src="/images/Python-Logo-4.svg" alt="Tableau" />
+      <img src="/images/Ubuntu-Logo-4.svg" alt="ubuntu" />
+      <img src="/images/IBM-Logo-2.svg" alt="IBM" /> 
+      <img src="/images/Gitlab-logo-1.svg" alt="GitLab" /> 
+      <img src="/images/ChatGPT-logo-5.svg" alt="ChatGPT" /> 
 
-    {/* Add more logos here */}
-  </div>
+      {/* Add more logos here */}
+    </div>
 </div>
   );
 }
