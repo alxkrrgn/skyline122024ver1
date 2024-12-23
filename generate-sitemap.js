e2d10d56e566cbb5bd61dc7d6d04e15b3c177e31
alxkrrgn <alxkrgn@hotmail.com>
2024-12-22 16:13:43 -0800

    updated sitemap.xml script and file

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -7,12 +7,15 @@ const { createWriteStream } = require('fs');
 
   sitemap.write({ url: '/', changefreq: 'daily', priority: 1.0 });
   sitemap.write({ url: '/strategies', changefreq: 'weekly', priority: 0.8 });
-  sitemap.write({ url: '/contact', changefreq: 'weekly', priority: 0.6 });
-  sitemap.write({ url: '/getstarted', changefreq: 'weekly', priority: 0.4 });
-  sitemap.write({ url: '/about', changefreq: 'weekly', priority: 0.3 });
-  sitemap.write({ url: '/stock-options', changefreq: 'weekly', priority: 0.5 });
+  sitemap.write({ url: '/contact', changefreq: 'weekly', priority: 0.7 });
+  sitemap.write({ url: '/getstarted', changefreq: 'weekly', priority: 0.7 });
+  sitemap.write({ url: '/stock-options', changefreq: 'weekly', priority: 0.7 });
   sitemap.write({ url: '/login', changefreq: 'weekly', priority: 0.9 });
-  sitemap.write({ url: '/register', changefreq: 'weekly', priority: 0.7 });
+  sitemap.write({ url: '/register', changefreq: 'weekly', priority: 0.9 });
+  sitemap.write({ url: '/getstarted', changefreq: 'weekly', priority: 0.9 });
+  sitemap.write({ url: '/about', changefreq: 'weekly', priority: 0.5 });
+  sitemap.write({ url: '/terms', changefreq: 'weekly', priority: 0.5 });
+  sitemap.write({ url: '/privacy', changefreq: 'weekly', priority: 0.5 });
 
   sitemap.end();
 
